c817f41a46a19076d9cb7785492a5a2ce54f7efb
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2019-03-11 00:07:21 +0000

    Exposed as local API and specific case for age verify app

--- predict.py
+++ predict.py
@@ -12,19 +12,20 @@ logger = logging.getLogger(__name__)
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-if __name__ == '__main__':
-    parser = ArgumentParser(add_help=True)
-    parser.add_argument('--model-dir', required=True)
-    parser.add_argument('--image-path', required=True)
+def predict_age(model_dir, image_path):
 
-    args = parser.parse_args()
-
-    prediction_fn = predictor.from_saved_model(export_dir=args.model_dir, signature_def_key='serving_default')
+    prediction_fn = predictor.from_saved_model(export_dir=model_dir, signature_def_key='serving_default')
 
     batch = []
 
-    image = imread(args.image_path)
+    image = imread(image_path)
     output = prediction_fn({
         'image': [image]
     })
-    print(output)
+    
+    return output["age_class"][0]
+
+
+if __name__ == "__main__":
+    predict_age('var/cnn-model/serving/1551650924/', '../303COM-Age-Verification-via-Facial-Recognition-App/photo.png')
+
